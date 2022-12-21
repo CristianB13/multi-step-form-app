595522efd331db14f5407d7d98832137ff6a8400
CristianB13 <cristian36200119@gmail.com>
2022-12-22 00:07:16 +0200

    change FormControls in plan FormGroup and update multi-step-form component template

--- src/app/components/multi-step-form/multi-step-form.component.ts
+++ src/app/components/multi-step-form/multi-step-form.component.ts
@@ -21,7 +21,7 @@ export class MultiStepFormComponent implements OnInit {
       }),
       plan: this.fb.group({
         type: ['', [Validators.required]],
-        option: ['monthly', [Validators.required]]
+        monthly: [true, [Validators.required]]
       }),
       addOns: this.fb.group({
         onlineService: [''],
